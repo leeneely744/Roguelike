c6088e9d65613c7507f67da299d3c6b95d979932
kohei sano <nlee9337@gmail.com>
2025-09-21 17:24:14 +0900

    Refactor food consumption logic to update food amount dynamically in GameManager and FoodObject

--- Assets/Scripts/FoodObject.cs
+++ Assets/Scripts/FoodObject.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class FoodObject : CellObject
 {
+    public int AmountGranted = 10;
+
     public override void PlayerEntered()
     {
         Destroy(gameObject);
 
-        Debug.Log("Food eaten!");
+        GameManager.Instance.ChangeFood(AmountGranted);
     }
 }
